87a0786d9eb545886ac10c6ba43327686e8e7d86
Marco Aring <marcoaring@yahoo.com.br>
2023-03-22 17:14:15 -0300

    refactor: refactor of delete method in repository

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,22 +1,4 @@
-const { v4 } = require('uuid');
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Marco',
-    email: 'marcoaring@marcoaring.com.br',
-    phone: '21985979964',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Kelvin',
-    email: 'kelvin@marcoaring.com.br',
-    phone: '21985979964',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -70,11 +52,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
